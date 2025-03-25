10e5463046f14f12922a6fcc516318b1bbfa4a98
Jerry Corum <corum@me.com>
2025-03-25 10:55:22 -0700

    intial variable correction

--- tf/aap-provider/main-aap.tf
+++ tf/aap-provider/main-aap.tf
@@ -13,7 +13,33 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "aap_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "aap_subnet" {
+  vpc_id            = aws_vpc.aap_vpc.id
+  cidr_block        = "172.16.10.0/24"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo2" {
+  subnet_id   = aws_subnet.aap_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface2"
+  }
 }
 
 resource "aws_instance" "tf-demo-aws-ec2-instance-2" {
@@ -21,13 +47,23 @@ resource "aws_instance" "tf-demo-aws-ec2-instance-2" {
   instance_type = "t2.micro"
   tags = {
     Name = "tf-demo-aws-ec2-instance-2"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo2.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
   }
 }
 
 provider "aap" {
-  host     = "https://controller"
+  host     = "https://gateway.sandbox3281.opentlc.com/"
   username = "admin"
-  password = "ansible123!"
+  password = "Ry3h0m3bus"
   insecure_skip_verify = true
 }
 

e5a3e66c4cdc4f1226464fec82dc24089f70cc4b
Jerry Corum <corum@me.com>
2025-03-25 10:25:18 -0700

    debug

--- tf/terraform/main.tf
+++ tf/terraform/main.tf
@@ -5,31 +5,46 @@ terraform {
       version = "5.84.0"
     }
   }
-  backend "s3" {}
+	backend "s3" {}
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Fetch the default VPC
-# data "aws_vpc" "ansible-vpc" {
-#   default = true
-# }
-resource "aws_vpc" "ansible-vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "aws_vpc" "aap_vpc" {
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "ansible"
+    Name = "tf-example"
   }
 }
 
-resource "aws_instance" "tf-demo-aws-ec2-instance-1" {
-  ami           = "ami-0005e0cfe09cc9050"
+resource "aws_subnet" "aap_subnet" {
+  vpc_id            = aap_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aap_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
-  tags = {
-    Name = "tf-demo-aws-ec2-instance-1"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
   }
 
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
 }
